1ba92434aca9be9811cde041a9f20c6e3ef6344f
Nathan Kester <natekester@Nates-MacBook-Pro.local>
2024-04-23 11:01:03 -0700

    add aliases file

---
+++ aliases.sh
@@ -0,0 +1,9 @@
+##the purpose of this is to provide a team aliases document - enabling everyone to have shared aliases across a team.
+# in some ways this would be replacing a makefile
+
+TODO_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+
+export PATH="$PATH:$TODO_DIR/bin"
+
+alias dc="docker-compose"
+alias testingAliases="echo 'yay'"
