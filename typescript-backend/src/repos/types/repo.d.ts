0d9d01532b904ef86d645f89bb58a619b402579b
Nathan Kester <natekester@Nates-MacBook-Pro.local>
2024-07-29 20:06:27 -0700

    remove unused where type

--- typescript-backend/src/repos/types/repo.d.ts
+++ typescript-backend/src/repos/types/repo.d.ts
@@ -1,11 +1,5 @@
 import { Knex } from "../../../node_modules/knex/types/index.js";
 
-type WhereType = Partial<{
-  id: string;
-  updatedAt: string;
-  createdAt: string;
-}>;
-
 export interface Repo<RecordType> {
   knx?: Knex;
   tableName?: Knex.TableDescriptor;
