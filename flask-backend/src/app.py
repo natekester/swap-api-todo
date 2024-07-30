5397a72bbfc9760add7f43ec7b759a37ffddcb0d
Nathan Kester <natekester@Nates-MacBook-Pro.local>
2024-07-29 17:23:24 -0700

    update python docker image, add both helloworld and get todos call

--- flask-backend/src/app.py
+++ flask-backend/src/app.py
@@ -1,16 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 import os
 
 app = Flask(__name__)
 
 @app.route("/api/hello-world")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def hello_world() :
+    return jsonify("Hello, World!")
 
 if __name__ == '__main__':
-    print("AHHHHHH")
     print(os.environ)
     port = os.environ["API_PORT"]
-
-    print("2AHHHHHH")
     app.run(host="0.0.0.0", port=port, debug=True)
--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect } from "react";
 import getAllTodos from "./api-calls/get-todo.api";
+import callHelloWorld from "./api-calls/hello-world.api";
 
 const App = ({ title }) => {
   useEffect(() => {
     async function fetchData() {
-      const res = await getAllTodos();
-      console.log({ res });
+      const helloWorldRes = await callHelloWorld();
+      console.log({ helloWorldRes });
+      const todosList = await getAllTodos();
+      console.log({ todosList });
     }
     fetchData();
   }, []);
